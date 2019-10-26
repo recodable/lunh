2540c1b8897a3bc0038e9acc23d64c6ef4cf0824
Steven Yung <stevenyung@icloud.com>
2019-10-26 11:20:30 +0200

    refactor for readability

--- index.ts
+++ index.ts
@@ -2,19 +2,21 @@ export default function lunh(value) {
   // Accept only digits, dashes or spaces
   if (/[^0-9-\s]+/.test(value)) return false;
 
-  let nCheck = 0,
-    bEven = false;
+  let nCheck = 0;
+  let bEven = false;
   value = value.replace(/\D/g, '');
 
-  for (var n = value.length - 1; n >= 0; n--) {
-    var cDigit = value.charAt(n),
-      nDigit = parseInt(cDigit, 10);
+  value
+    .split('')
+    .reverse()
+    .forEach(char => {
+      let nDigit = parseInt(char, 10);
 
-    if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+      if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
 
-    nCheck += nDigit;
-    bEven = !bEven;
-  }
+      nCheck += nDigit;
+      bEven = !bEven;
+    });
 
   return nCheck % 10 == 0;
 }
