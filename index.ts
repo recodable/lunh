f116c6ec01513de0c97e5c83b5c79500e4263d43
Steven Yung <stevenyung@icloud.com>
2019-10-27 09:26:49 +0100

    refactor using reduce

--- index.ts
+++ index.ts
@@ -2,21 +2,20 @@ export default function lunh(value) {
   // Accept only digits, dashes or spaces
   if (/[^0-9-\s]+/.test(value)) return false;
 
-  let nCheck = 0;
   let bEven = false;
   value = value.replace(/\D/g, '');
 
-  value
+  const nCheck = value
     .split('')
     .reverse()
-    .forEach(char => {
+    .reduce((nCheck, char) => {
       let nDigit = parseInt(char, 10);
 
       if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
 
-      nCheck += nDigit;
       bEven = !bEven;
-    });
+      return nCheck + nDigit;
+    }, 0);
 
   return nCheck % 10 == 0;
 }
